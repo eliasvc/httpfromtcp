1ccf6e117127d3cf060303de34b2f91a939b51c7
Elias  Valdez <elias.vc@gmail.com>
2025-03-19 14:53:55 -0400

    Print entire lines now instead of 8 byte chunks

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,15 +16,23 @@ func main() {
 	defer f.Close()
 	// Where we should start reading from
 	var offset int64
+	var currentLine string
 	buffer := make([]byte, 8)
 	for {
 		n, err := f.ReadAt(buffer, offset)
-		fmt.Printf("read: %s\n", buffer[:n])
+		parts := strings.Split(string(buffer[:n]), "\n")
+		if len(parts) == 1 {
+			currentLine += parts[0]
+		}
+		if len(parts) == 2 {
+			currentLine += parts[0]
+			fmt.Printf("read: %s\n", currentLine)
+			currentLine = parts[1]
+		}
 
 		if err == io.EOF {
 			break
 		}
-
 		offset += int64(n)
 	}
 }
