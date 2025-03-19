b55222d5ad96829f6184acf0e540bcd20131ab94
Elias  Valdez <elias.vc@gmail.com>
2025-03-18 22:22:27 -0400

    Read messages 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	// Where we should start reading from
+	var offset int64
+	buffer := make([]byte, 8)
+	for {
+		n, err := f.ReadAt(buffer, offset)
+		fmt.Printf("read: %s\n", buffer[:n])
+
+		if err == io.EOF {
+			break
+		}
+
+		offset += int64(n)
+	}
 }
