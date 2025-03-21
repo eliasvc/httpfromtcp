633f3bc4e6a4c6cd436e0da283760c9b94e5f44c
Elias  Valdez <elias.vc@gmail.com>
2025-03-21 09:42:35 -0400

    Get file reading into its own function and make it concurrent

--- main.go
+++ main.go
@@ -14,25 +14,42 @@ func main() {
 		log.Fatal(err)
 	}
 	defer f.Close()
-	// Where we should start reading from
-	var offset int64
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+
+	lines := make(chan string)
 	var currentLine string
 	buffer := make([]byte, 8)
-	for {
-		n, err := f.ReadAt(buffer, offset)
-		parts := strings.Split(string(buffer[:n]), "\n")
-		if len(parts) == 1 {
-			currentLine += parts[0]
-		}
-		if len(parts) == 2 {
-			currentLine += parts[0]
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = parts[1]
-		}
+	go func() {
+		for {
+			n, err := f.Read(buffer)
+			parts := strings.Split(string(buffer[:n]), "\n")
+			// If len == 1 then no \n was encountered
+			if len(parts) == 1 {
+				currentLine += parts[0]
+			}
+			// When a \n is encountered, we want to concat whatever we already have in currentLine
+			// plus the first part, which forms a complete line. Then we'll want to reset currentLine
+			// with the next part. Every part of the slice is actually a complete line, except for the
+			// start and the end.
+			if len(parts) > 1 {
+				currentLine += parts[0]
+				for i := 1; i < len(parts); i++ {
+					lines <- currentLine
+					currentLine = parts[i]
+				}
+			}
 
-		if err == io.EOF {
-			break
+			if err == io.EOF {
+				close(lines)
+				break
+			}
 		}
-		offset += int64(n)
-	}
+	}()
+	return lines
 }
