649b2151397d376abf0ae6f0100a82cfbe2e2441
Elias  Valdez <elias.vc@gmail.com>
2025-05-16 17:45:19 -0400

    Read chunks of 8 bytes from messages and print them

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
+const bufSize = 8
+
 func main() {
-	fmt.Println("I hope I get the job!")
+	buf := make([]byte, bufSize, bufSize)
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	for {
+		n, err := f.Read(buf)
+		if err != nil && err != io.EOF {
+			log.Fatalf("Error reading the file: %q\n", err)
+		}
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Printf("read: %s\n", buf[0:n])
+	}
+
 }
