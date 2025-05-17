de03a26e1a400bca50263967eb83654a3e2c33b9
Elias  Valdez <elias.vc@gmail.com>
2025-05-17 17:27:58 -0400

    Move file read and line chunking into function, which runs it in goroutine

--- main.go
+++ main.go
@@ -11,37 +11,49 @@ import (
 const bufSize = 8
 
 func main() {
-	buf := make([]byte, bufSize, bufSize)
-	var currentLine string
 
 	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
-	for {
-		n, err := f.Read(buf)
-		if err != nil && err != io.EOF {
-			log.Fatalf("Error reading the file: %q\n", err)
-		}
-		if err == io.EOF {
-			break
-		}
-		sBuf := string(buf[0:n])
-		parts := strings.Split(sBuf, "\n")
-		// The last part will be the one not ending on '\n', so the loop doesn't need
-		// to include it
-		for i := range len(parts) - 1 {
-			currentLine += parts[i]
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = ""
-		}
+	ch := getLinesChannel(f)
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
+	}
+}
 
-		// Add the last part, which doesn't include '\n'
-		currentLine += parts[len(parts)-1]
-	}
-	if currentLine != "" {
-		fmt.Printf("read: %s\n", currentLine)
-	}
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		buf := make([]byte, bufSize, bufSize)
+		var currentLine string
+		for {
+			n, err := f.Read(buf)
+			if err != nil && err != io.EOF {
+				log.Fatalf("Error reading the file: %q\n", err)
+			}
+			if err == io.EOF {
+				close(out)
+				break
+			}
+			sBuf := string(buf[0:n])
+			parts := strings.Split(sBuf, "\n")
+			// The last part will be the one not ending on '\n', so the loop doesn't need
+			// to include it
+			for i := range len(parts) - 1 {
+				currentLine += parts[i]
+				out <- currentLine
+				currentLine = ""
+			}
+
+			// Add the last part, which doesn't include '\n'
+			currentLine += parts[len(parts)-1]
+		}
+		if currentLine != "" {
+			out <- currentLine
+		}
+	}()
+	return out
 }
