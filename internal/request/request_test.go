7a0f9c7299e43f48ed2d4559da19b1d8c13ca8bb
Elias  Valdez <elias.vc@gmail.com>
2025-05-25 12:43:45 -0400

    Added more test cases for request line

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -18,14 +18,35 @@ func TestRequestLineParse(t *testing.T) {
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
 	// Test: Good GET Request line with path
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	r, err = RequestFromReader(
+		strings.NewReader(
+			"GET /coffee HTTP/1.1\r\n" +
+				"Host: localhost:42069\r\n" +
+				"User-Agent: curl/7.81.0\r\n" +
+				"Accept: */*\r\n" +
+				"Content-Type:application-json\r\n" +
+				"Content-Length: 22\r\n\r\n" +
+				"{\"flavor\":\"dark mode\"}",
+		),
+	)
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
 	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
+	// Test: Valid POST request
+	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+
 	// Test: Invalid number of parts in request line
 	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: request line parts in wrong order
+	_, err = RequestFromReader(strings.NewReader("/coffee GET HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid HTTP version
+	_, err = RequestFromReader(strings.NewReader("/coffee GET HTTP/2.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
 }
