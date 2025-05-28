10f8642e0e6e7376ec026b320ed313ef7c989a0f
Elias  Valdez <elias.vc@gmail.com>
2025-05-28 17:19:37 -0400

    Refactored parseRequestLine so it harbors the CRLF finding logic and uses the raw input bytes)

--- internal/request/request.go
+++ internal/request/request.go
@@ -23,15 +23,8 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Error reading request: %q\n", err.Error())
 	}
-	sRequest := string(inputRequest)
-	requestParts := strings.Split(sRequest, "\r\n")
 
-	// '\r\n' not found in request
-	if len(requestParts) == 1 {
-		return nil, fmt.Errorf("Malformed request. '\\r\\n' not found: %q\n", sRequest)
-	}
-
-	request.RequestLine, err = parseRequestLine(requestParts[0])
+	request.RequestLine, err = parseRequestLine(inputRequest)
 	if err != nil {
 		return nil, fmt.Errorf("Error parsing request-line: %q\n", err)
 	}
@@ -39,34 +32,40 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	return &request, nil
 }
 
-func parseRequestLine(inputRequestLine string) (RequestLine, error) {
+func parseRequestLine(inputRequest []byte) (RequestLine, error) {
 	var requestLine RequestLine
 	validMethod := regexp.MustCompile(`[A-Z]+`)
 	validHTTPVersion := "HTTP/1.1"
 
-	parts := strings.Split(inputRequestLine, " ")
+	inputRequestText := string(inputRequest)
+	crlfIndex := strings.Index(inputRequestText, "\r\n")
+	if crlfIndex == -1 {
+		return requestLine, fmt.Errorf("CRLF not found on request-line")
+	}
+	requestLineText := inputRequestText[:crlfIndex]
+
+	parts := strings.Split(requestLineText, " ")
 	// request-line is composed of "method SP request-target SP HTTP-version"
 	// where SP is single space. So it has to have only 3 parts if devided by
 	// space
 	if len(parts) != 3 {
-		return requestLine, fmt.Errorf("Malformed request-line: %q\n", inputRequestLine)
+		return requestLine, fmt.Errorf("Malformed request-line: %q\n", requestLineText)
 	}
 
 	method, target, version := parts[0], parts[1], parts[2]
 
 	if !validMethod.MatchString(method) {
-		return requestLine, fmt.Errorf("Malformed method in request-line: %q\n", inputRequestLine)
+		return requestLine, fmt.Errorf("Malformed method in request-line: %q\n", requestLineText)
 	}
 
-	requestLine.Method = method
-
-	requestLine.RequestTarget = target
-
 	if validHTTPVersion != version {
-		return requestLine, fmt.Errorf("Malformed HTTP version in request-line: %q\n", inputRequestLine)
+		return requestLine, fmt.Errorf("Malformed HTTP version in request-line: %q\n", requestLineText)
 	}
 	versionParts := strings.Split(version, "/")
 	httpVersion := versionParts[1]
+
+	requestLine.Method = method
+	requestLine.RequestTarget = target
 	requestLine.HttpVersion = httpVersion
 
 	return requestLine, nil
