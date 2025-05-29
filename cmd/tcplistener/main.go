17fb09eac95602aa936199772da43d6c67f16e51
Elias  Valdez <elias.vc@gmail.com>
2025-05-29 17:48:33 -0400

    Modify tcplistener so it uses http parsing code. Removed outdated code

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"httpfromtcp/internal/request"
 )
 
 const bufSize = 8
@@ -25,46 +25,14 @@ func main() {
 		}
 		fmt.Println("Connection accepted")
 
-		ch := getLinesChannel(conn)
-		for line := range ch {
-			fmt.Printf("read: %s\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Println(err)
+			continue
 		}
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string)
-
-	go func() {
-		buf := make([]byte, bufSize, bufSize)
-		var currentLine string
-		defer f.Close()
-		defer close(out)
-		for {
-			n, err := f.Read(buf)
-			if err != nil && err != io.EOF {
-				log.Fatalf("Error reading the file: %q\n", err)
-			}
-			if err == io.EOF {
-				// Dump whatever was read when EOF hit
-				if currentLine != "" {
-					out <- currentLine
-				}
-				break
-			}
-			sBuf := string(buf[0:n])
-			parts := strings.Split(sBuf, "\n")
-			// The last part will be the one not ending on '\n', so the loop doesn't need
-			// to include it
-			for i := range len(parts) - 1 {
-				currentLine += parts[i]
-				out <- currentLine
-				currentLine = ""
-			}
-
-			// Add the last part, which doesn't include '\n'
-			currentLine += parts[len(parts)-1]
-		}
-	}()
-	return out
 }
